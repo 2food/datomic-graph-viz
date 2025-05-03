ef5eec3f62c9e1dc42261b6217b602e3b6aa3373
Tormod Mathiesen <tormathi@gmail.com>
2025-05-03 20:38:12 +0200

    automatically open url

--- src/datomic_viz/main.clj
+++ src/datomic_viz/main.clj
@@ -2,14 +2,23 @@
   (:require [babashka.cli :as cli]
             [bling.core :as bling]
             [datomic-viz.server]
-            [mount.core :as m]))
+            [mount.core :as m])
+  (:import (java.awt Desktop)
+           (java.net URI)))
+
+(defn open-url [url]
+  (try (.browse (Desktop/getDesktop) (URI. url))
+       (catch Exception _
+         (println (str "Could not automatically open URL.")))))
 
 (def cli-opts {:coerce     {:port                :long
                             :max-edges-per-level :long}
                :exec-args  {:port                1234
                             :max-edges-per-level 100}
                :args->opts [:conn-str]
-               :require    [:conn-str]})
+               :require    [:conn-str]
+               :restrict   [:conn-str :port :max-edges-per-level]})
+
 
 (defn -main [& args]
   (let [args (cli/parse-opts args cli-opts)]
@@ -18,4 +27,5 @@
     (m/start-with-args args)
     (bling/callout {:type  :positive
                     :label "Started"}
-                   (str "Running at http://localhost:" (:port args)))))
+                   (str "Running at http://localhost:" (:port args)))
+    (open-url (str "http://localhost:" (:port args)))))
